8e28b5626b7d18c5cc95008ccb39483b270babe4
kangjian <kangjiantsui@163.com>
2023-03-07 17:02:31 +0800

    tcp client,记录登陆resp的uid,供后续请求使用,调通

--- go/tcpclient-dtfish/tcp_client_test.go
+++ go/tcpclient-dtfish/tcp_client_test.go
@@ -5,10 +5,13 @@ import (
 	"QuickStart/go/tcpclient-dtfish/net/codec"
 	tcp2 "QuickStart/go/tcpclient-dtfish/net/tcp"
 	pb "QuickStart/go/tcpclient-dtfish/proto"
+	dtpb "QuickStart/go/tcpclient-dtfish/proto/dtfish"
 	"QuickStart/go/utils"
 	"encoding/binary"
 	"fmt"
+	"github.com/golang/protobuf/proto"
 	"net"
+	"sync"
 	"testing"
 )
 
@@ -44,27 +47,75 @@ func TestTcpClient(t *testing.T) {
 	utils.PrintJson(conn.LocalAddr())
 	session := tcp2.NewSession(
 		conn,
-		tcp2.Session_type_tcp,
+		tcp2.SessionTypeTcp,
 		tcp2.NewMsgParser(),
 		&codec.DTFishClientProtoProtocol{Endian_: binary.LittleEndian},
 		2000,
 	)
-	err = session.Send(&common.ProtocolClientHead{
-		Uid_:    111,
-		Seq_:    100,
-		Msg_id_: uint32(pb.EmCSMsgId_CS_MSG_PLAYER_LOGIN),
-	}, &pb.PbCsPlayerLoginReqMsg{LoginType: pb.EmLoginType_Login_type_dt})
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	var uid uint64
+	go func() {
+		for {
+			msg, _ := session.Receive()
+			if msg == nil {
+				continue
+			}
+			resp := msg.([]interface{})
+			uid = resp[0].(*common.ProtocolClientHead).Uid_
+			t.Log(uid)
+			wg.Done()
+			break
+		}
+	}()
+	loginType := dtpb.EnumLoginType_enumLoginTypeToRegisterNewUser
+	gameType := dtpb.EnumGameType_enumGameTypeDouDiZhu
+	account := "asadsadsasdasdads"
+	password := "asadsadsasdasdads"
+	nickname := "asadsadsasdasdads"
+	gender := dtpb.EnumGender_enumGenderMale
+	deviceType := dtpb.EnumDeviceType_enumDeviceTypeAndroid
+	deviceID := ""
+	macAddress := ""
+	secureKey := ""
+	channel := "xxxs"
+	var version uint32 = 30000001
+	dtlq := &dtpb.LoginRequest{
+		LoginType:  &loginType,
+		Account:    &account,
+		GameType:   &gameType,
+		Password:   &password,
+		Nickname:   &nickname,
+		Gender:     &gender,
+		DeviceType: &deviceType,
+		DeviceID:   &deviceID,
+		MacAddress: &macAddress,
+		SecureKey:  &secureKey,
+		Channel:    &channel,
+		Version:    &version,
+	}
+	loginData, err := proto.Marshal(dtlq)
 	if err != nil {
-		t.Fatalf(`Send报错,err:%s`, err.Error())
+		t.Fatal(err.Error())
 	}
 	err = session.Send(&common.ProtocolClientHead{
 		Uid_:    111,
 		Seq_:    100,
-		Msg_id_: uint32(pb.EmCSMsgId_CS_MSG_GAME_ACTIVITY_PLAYER_INFO),
-	}, &pb.PBCsGameActivityPlayerInfoReqMsg{
-		ActivityId: []int32{
-			249,
-		},
+		Msg_id_: uint32(pb.EmCSMsgId_CS_MSG_PLAYER_LOGIN),
+	}, &pb.PbCsPlayerLoginReqMsg{
+		LoginType:       pb.EmLoginType_Login_type_dt,
+		DtfishLoginData: loginData,
+	})
+	if err != nil {
+		t.Fatalf(`Send报错,err:%s`, err.Error())
+	}
+	wg.Wait()
+	err = session.Send(&common.ProtocolClientHead{
+		Uid_:    uid,
+		Seq_:    100,
+		Msg_id_: uint32(pb.EmCSMsgId_CS_MSG_GAME_SHOP_EXCHANGE_DELIVERY),
+	}, &pb.PBCsGameShopExchangeDeliveryReqMsg{
+		ProductId: 1111,
 	})
 	if err != nil {
 		t.Fatalf(`Send报错,err:%s`, err.Error())
