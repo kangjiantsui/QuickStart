280ae3b1e062a67bc060da97aaa599bd8cef4b9f
kangjian <kangjiantsui@163.com>
2023-03-21 19:11:49 +0800

    除妖试炼,配置

--- go/leetcode/easy_test.go
+++ go/leetcode/easy_test.go
@@ -1,6 +1,9 @@
 package leetcode
 
-import "testing"
+import (
+	"QuickStart/go/utils"
+	"testing"
+)
 
 func twoSum(nums []int, target int) []int {
 	for i := 0; i < len(nums); i++ {
@@ -16,4 +19,24 @@ func twoSum(nums []int, target int) []int {
 func TestTwoSum(t *testing.T) {
 	result := twoSum([]int{2, 7, 11, 15}, 9)
 	t.Log(result)
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+
+	return nil
+}
+
+// https://leetcode.com/problems/add-two-numbers/
+func TestAddTwoNumbers(t *testing.T) {
+	l1 := &ListNode{2, &ListNode{4, &ListNode{3, nil}}}
+	l2 := &ListNode{5, &ListNode{6, &ListNode{4, nil}}}
+	utils.PrintJson(l1)
+	utils.PrintJson(l2)
+	l3 := addTwoNumbers(l1, l2)
+	utils.PrintJson(l3)
 }
