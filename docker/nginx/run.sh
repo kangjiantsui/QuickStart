19c0ed8872e4990c441a7e45b5b334ca7c1aaca7
kangjian <690656728@qq.com>
2023-09-18 10:38:09 +0800

    docker,修改nginx端口为10080,避免冲突

--- docker/nginx/run.sh
+++ docker/nginx/run.sh
@@ -1,1 +1,1 @@
-docker run -d -p 8080:8080 --name my-nginx -v C:\\workspace\\QuickStart\\docker\\nginx\\nginx:/etc/nginx nginx
+docker run -d -p 10080:10080 --name my-nginx -v C:\\workspace\\QuickStart\\docker\\nginx\\nginx:/etc/nginx nginx
