9d00d420fb648f715dec64f8c2493686279fced9
kangjian <690656728@qq.com>
2024-02-27 13:43:45 +0800

    docker,nginx,添加ro_support_tool

--- docker/nginx/run.sh
+++ docker/nginx/run.sh
@@ -1,1 +1,5 @@
-docker run -d -p 18080:18080 --name my-nginx -v C:\\workspace\\QuickStart\\docker\\nginx\\nginx:/etc/nginx nginx
+docker rm -f my-nginx
+docker run -d -p 18080:18080 --name my-nginx \
+-v C:\\workspace\\QuickStart\\docker\\nginx\\nginx:/etc/nginx \
+-v C:\\workspace\\ro_support_tool\\public:/var/www/ro_support_tool \
+nginx
